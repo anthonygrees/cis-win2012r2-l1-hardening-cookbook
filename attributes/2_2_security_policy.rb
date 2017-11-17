21ea78fb7e28671a7f036dfe138d9b40bfb8a6cc
Matt Ray <matthewhray@gmail.com>
2017-11-18 09:13:02 +1100

    support for the 2.2 security policies

---
+++ attributes/2_2_security_policy.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+#
+# Cookbook Name:: cis-win2012r2-l1-hardening
+# Attributes:: 2_2_security_policy
+#
+
+# CIS 2.2.18 'Deny log on as a batch job' to include 'Guests'
+default['security_policy']['rights']['SeDenyBatchLogonRight']             = '*S-1-5-32-546'
+# CIS 2.2.19 'Deny log on as a service' to include 'Guests'
+default['security_policy']['rights']['SeDenyServiceLogonRight']           = '*S-1-5-32-546'
+# CIS 2.2.20 'Deny log on locally' to include 'Guests'
+default['security_policy']['rights']['SeDenyInteractiveLogonRight']       = '*S-1-5-32-546'
+# CIS 2.2.21 'Deny log on through Remote Desktop Services' to include 'Guests, Local account'
+default['security_policy']['rights']['SeDenyRemoteInteractiveLogonRight'] = '*S-1-5-32-546, *S-1-5-113'
